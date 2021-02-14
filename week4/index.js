850124fb18e7d50e7839776fe1f454f9b7e5cfdf
michaelkosub/fsw-100 <michael.kosub@bryanuniversity.edu>
2021-02-14 16:15:30 -0600

    first commit

--- week4/index.js
+++ week4/index.js
@@ -1,18 +1,26 @@
-axios.get("https://api.vschool.io/horses/todo")
-    .then(response => {
-        for(let i = 0; i < response.data.length; i++){
-            const h3 = document.createElement('h3')
-            if (response.data[i].completed === true){
-                h3.textContent = response.data[i].title
-                h3.style.textDecoration = "line-through"
-                h3.style.textDecorationColor = "black"
-                h3.style.textAlign = "center"
-                h3.style.color="blue"
-            } else {
-                h3.textContent = `${response.data[i].title}`
-                h3.style.textAlign = "center"
-            }
-                document.body.appendChild(h3)
+function films() {
+    axios.get("http://swapi.dev/api/films/")
+    .then((response) => {
+        for (i = 0; i < response.data.results.length; i++) {
+            const h1 = document.createElement("h1")
+            h1.innerHTML = `${response.data.results[i].title}`
+            let data = document.getElementById("list")
+            data.append(h1)
         }
     })
-    .catch(error => console.log(error)
+    .catch ((error) => alert("error"));
+}
+
+function species() {
+    axios.get("http://swapi.dev/api/species/")
+    .then((response) => {
+        for (i = 0; i < response.data.results.length; i++) {
+            const h1 = document.createElement("h1")
+            h1.innerHTML = `${response.data.results[i].name}`
+            let data = document.getElementById("list")
+            data.append(h1)
+        }
+    })
+    .catch ((error) => alert("error"));
+}
+
